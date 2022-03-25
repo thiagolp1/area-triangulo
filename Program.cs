1574acf4bb9ca736a6e1028f44da6ccac7c43663
Thiago <Thiagolopespereiralp1@gmail.com>
2022-03-25 20:29:44 -0300

    finalição do exercicio

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿double H, B, area;
+
+Console.Write("Base....: ");
+B = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Altura..: ");
+H = Convert.ToDouble(Console.ReadLine());
+
+area = (B * H) / 2;
+
+Console.WriteLine($"\nÁrea....: {area:N3}");
+
